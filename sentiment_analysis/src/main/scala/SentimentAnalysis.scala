22493485324dae21ee6eded0733280a03b21d172
spark131008 <spark131008@gmail.com>
2021-02-11 23:49:09 -0500

    sentiment analysis updated

--- sentiment_analysis/src/main/scala/SentimentAnalysis.scala
+++ sentiment_analysis/src/main/scala/SentimentAnalysis.scala
@@ -26,54 +26,37 @@ object SentimentAnalysis{
         .option("inferSchema", "true")
         .parquet("/datalake2/*").cache()
 
-    val newValueDF = tweetDF.withColumn("newValue", $"text".cast(StringType)).select($"newValue")
-    val textToArray = newValueDF.collectAsList.toArray
-    var mappingTextAndAnalysis = ListBuffer[Map[String, String]]()
+    val testDF = tweetDF.withColumn("text", $"text".cast(StringType)).select($"text")
+
+    val textToArray = testDF.collectAsList.toArray
+
+    var listOfAalysisResult = ListBuffer[String]()
     for(i <- textToArray){
           val analysisResult = sentimentAnalysis(i.toString, spark)
-          mappingTextAndAnalysis += Map(i.toString -> analysisResult)
+          listOfAalysisResult += analysisResult
     }
 
-    val sentimentResultToList = mappingTextAndAnalysis.toDF()
-    val finalDF = sentimentResultToList.select((map_keys($"value"))(0) as "Text", (map_values($"value"))(0) as "Sentiment-Analysis")
-
-    val finalDF2 = finalDF.groupBy("Sentiment-Analysis").count()
-    val finalDF3 = finalDF2.groupBy("Sentiment-Analysis", "count" ).agg(sum("count") as "Sum" ) .withColumn("Percentage", $"count" / sum("Sum").over())
-    val finalDF4 = finalDF3.select($"Sentiment-Analysis", $"Sum", round(($"Percentage" * 100), 2) as "Percentage(%)").orderBy(desc("Sentiment-Analysis")).toDF()
+    val finalDF = listOfAalysisResult.toDF("Sentiment-Analysis")
+    val finalDF2 = finalDF.groupBy($"Sentiment-Analysis").count()
+    val finalDF3 = finalDF2.groupBy("Sentiment-Analysis", "count").agg(sum("count") as "Sum" ).withColumn("Percentage", $"count" / sum("Sum").over())
+    val finalDF4 = finalDF3.select($"Sentiment-Analysis", $"Sum", round(($"Percentage" * 100), 2) as "Percentage(%)").orderBy(desc("Percentage(%)"))
     finalDF4.show(false)
-
+    spark.stop()
   }
   def sentimentAnalysis(text: String, spark: SparkSession): String = {
         import spark.implicits._
         //Read a text file and convert it into Dataframe
         val sentimentLexicon = spark.read
-            .option("inferSchema", "true")
-            .textFile("/Subjectivity_Lexicon.txt").cache()
+            .option("delimiter", " ")
+            .csv("/datalake/Subjectivity_Lexicon.txt")
+            .toDF("type", "length", "word", "pos1", "stemmed1", "sentiment").cache()
 
-        val lexiconToArray = sentimentLexicon.collectAsList.toArray
-        var mappingWordAndSentiment = ListBuffer[Map[String, String]]()
-        for(i <- lexiconToArray){
-            val words = i.toString.split(" ")
-            var word1 = ""
-            var sentiment1 = ""
-            for(j <- words){
-                if(j.contains("word1")) {
-                    word1 = j.split("=")(1)
-                }else if(j.contains("priorpolarity")) {
-                    sentiment1 = j.split("=")(1)
-                }
-            }
-            mappingWordAndSentiment += Map(word1 -> sentiment1)
-        }
-
-        val lexiconToList = mappingWordAndSentiment.toDF()
-        val finalDF = lexiconToList.select((map_keys($"value"))(0) as "Word", (map_values($"value"))(0) as "Sentiment")
-        val removeDuplicateDF = finalDF.dropDuplicates("Word")
-        val textToWords = text.split(" ")
+        val lexiconDF = sentimentLexicon.select(split(col("word"), "=")(1).as("Word"), split(col("sentiment"), "=")(1).as("Sentiment"))
+        val removeDuplicateDF = lexiconDF.dropDuplicates("Word")
+        val textToWords = text.split(", ")
 
         if(textToWords.size <= 1){
-            val filterDF = removeDuplicateDF.filter($"Word" === (text))
-            val result = filterDF.select($"Sentiment").collect().map(_.getString(0)).mkString(" ")
+            val result = removeDuplicateDF.filter($"Word" === (text)).select($"Sentiment").collect().map(_.getString(0)).mkString(" ")
             if(result.isEmpty){
                 return "mixed"
             }else{
@@ -82,11 +65,10 @@ object SentimentAnalysis{
         }else{
             var scoreCount = 0
             textToWords.foreach{ x =>
-                val filterDF = removeDuplicateDF.filter($"Word" === (x))
-                val result = filterDF.select($"Sentiment").collect().map(_.getString(0)).mkString(" ")
+                val result = removeDuplicateDF.filter($"Word" === (x)).select($"Sentiment").collect().map(_.getString(0)).mkString(" ")
                 if(result.equals("positive")) scoreCount += 1
                 else if(result.equals("negative")) scoreCount -=1
-                else if(result.isEmpty) scoreCount += 0 
+                else scoreCount += 0 
             }
             if(scoreCount > 0) return "positive"
             else if(scoreCount < 0) return "negative"
